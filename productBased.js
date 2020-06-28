ff2ca6659c59fab7ac104aabe2a520d37d543f4b
shashikanth8639 <49157671+shashikanth8639@users.noreply.github.com>
2020-06-28 20:27:40 +0530

    Update productBased.js

--- productBased.js
+++ productBased.js
@@ -1,27 +1,10 @@
-const express = require('express')
-
-const Company = require('../models/company')
-
-const router = express.Router()
+//routes for product based companies backend
 
 router.get('/productBased', async(req,res)=>{
     const companies = await Company.find({isProductBased:true})
     // console.log(companies)
     try{
         res.send(companies)
-    }catch(e){
-        res.status(500).send(e)
-    }
-})
-
-router.get('/pBased', async(req,res)=>{
-    try{
-        const company = await Company.find({isProductBased:true}, {_id:1,imageLink:1,name:1})
-        // console.log(company)
-        if(!company){
-            return res.status(404).send()
-        }
-        res.send(company)
     }catch(e){
         res.status(500).send(e)
     }
@@ -38,5 +21,3 @@ router.get('/productBased/:id', async(req,res)=>{
         res.status(500).send(e)
     }
 })
-
-module.exports = router
