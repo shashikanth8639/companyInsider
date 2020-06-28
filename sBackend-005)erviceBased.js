c69a27f6c3f68ffb3e71e3f2a1a05139cfe3d8af
shashikanth8639 <49157671+shashikanth8639@users.noreply.github.com>
2020-06-28 20:33:41 +0530

    Update and rename serviceBased.js to sBackend-005)erviceBased.js

--- sBackend-005)erviceBased.js
+++ sBackend-005)erviceBased.js
@@ -1,26 +1,9 @@
-const express = require('express')
-
-const Company = require('../models/company')
-
-const router = express.Router()
+// code for service based routes
 
 router.get('/serviceBased', async(req,res)=>{
     const companies = await Company.find({isProductBased:false})
     try{
         res.send(companies)
-    }catch(e){
-        res.status(500).send(e)
-    }
-})
-
-
-router.get('/sBased', async(req,res)=>{
-    try{
-        const company = await Company.find({isProductBased:false}, {_id:1,imageLink:1,name:1})
-        if(!company){
-            return res.status(404).send()
-        }
-        res.send(company)
     }catch(e){
         res.status(500).send(e)
     }
@@ -37,5 +20,3 @@ router.get('/serviceBased/:id', async(req,res)=>{
         res.status(500).send(e)
     }
 })
-
-module.exports = router
